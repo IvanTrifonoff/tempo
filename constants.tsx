7a399c8982dfa9b2d7fca246f876e4af5cfd678f
Ivan T <i@trfnv.ru>
2026-02-12 05:17:27 -0800

    Build: Align APP_VERSION with CHANGELOG for CI/CD (v1.0.46)

--- constants.tsx
+++ constants.tsx
@@ -1,6 +1,6 @@
 import { DanceStyle, Track } from './types';
 
-export const APP_VERSION = '1.0.43';
+export const APP_VERSION = '1.0.46';
 
 export const INITIAL_TRACKS: Track[] = [
   // ... (оставляю как есть)
